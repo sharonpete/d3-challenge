dde894c03e040bf684f01121829887464fa26008
Sharon Templin <sharon@hollygo.es>
2021-04-26 21:13:49 -0500

    Initial SVG layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,30 @@
 console.log("app.js loaded");
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select div "scatter", append SVG area to it, and set the dimensions
+var svg = d3.select("#scatter")
+            .append("svg")
+            .attr("height", svgHeight)
+            .attr("width", svgWidth);
+
+// Append a group to the SVG area and 'translate' it to
+// the margins set in the 'chartMargin' object
+var chartGroup = svg.append("g")
+            .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
 
 function init() {
@@ -30,7 +56,7 @@ function init() {
         console.log(`Smokes: ${smokes}`);
         console.log(`SmokesLow: ${smokesLow}`);
         console.log(`SmokesHigh: ${smokesHigh}`);
-
+        
 
     });
 }
