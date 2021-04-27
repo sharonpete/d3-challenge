59e01cd84658575990212a91d1632cf10af60dc1
Sharon Templin <sharon@hollygo.es>
2021-04-26 20:39:57 -0500

    CSV Data loaded successfully.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,9 @@ console.log("app.js loaded");
 
 function init() {
     console.log("init() called");
-
+    d3.csv("../../assets/data/data.csv").then(function(data){
+        console.log(data);
+    });
 }
 
 init();
